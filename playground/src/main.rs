82081ff747224b69b9cbd099b40749b33371c6d3
tokeiya <netseed@seedcreate.onmicrosoft.com>
2024-02-25 17:41:20 +0900

    fix infix minimal formatter.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,49 +1,45 @@
-use std::io::{Read, Write};
-use std::ops::Index;
-
 use anyhow::{Context, Result};
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::ops::Index;
 
 use parser::infix::parser::parse;
 use regex::Regex;
+use syntax::arithmetic_expression::ArithmeticExpression;
 use syntax::dot_writer::write_dot;
 
 mod html_writer;
 
 fn main() {
-	const INPUT: &str = r"hello
-world";
-
-	let re = Regex::new(r"(?m)&.*$").unwrap();
-	for caps in re.captures_iter(INPUT) {
-		println!("First word: {}, Second word: {}", &caps[1], &caps[2]);
-	}
-
-	// let tree = parse("1+2*{3+4}").unwrap().0;
-	// let mut cursor = std::io::Cursor::<Vec<u8>>::default();
-	//
-	// write_dot(&mut cursor, &tree).unwrap();
-	// let str = String::from_utf8(cursor.into_inner()).unwrap();
-	// let str = html_writer::generate_svg(&str).unwrap();
-	//
-	// let reg = regex::Regex::new(r"(?m)^(.*)$").unwrap();
-	//
-	// println!("{}", &str);
-	//
-	// let cap = reg.captures(&str).unwrap();
-	//
-	// for elem in cap.iter() {
-	// 	println!("{:?}", elem)
-	// }
+	check();
 }
 
-fn bar() {
-	let re = regex::Regex::new(r"(?m)^(.*)$").unwrap();
-	let text = "First line\nSecond line\nThird line";
+fn check() {
+	let tree = parse("{{{1+2*3}/{{4-5}*{{6+7}/2}}}}").unwrap().0;
+	let minimal = parser::infix::formatter::minimal_infix_notation(&tree);
+	println!("{}", &minimal);
+}
 
-	for cap in re.captures_iter(text) {
-		if let Some(matched) = cap.get(1) {
-			// キャプチャグループ1にアクセス
-			println!("Captured line: {}", matched.as_str());
-		}
-	}
+fn write_sample_dot() {
+	let tree = parse("{1+2*3}/{{4-5}*{{6+7}/2}}").unwrap().0.simplify();
+	let mut cursor = std::io::Cursor::<Vec<u8>>::default();
+
+	syntax::dot_writer::write_dot(&mut cursor, &tree).unwrap();
+	println!("{}", String::from_utf8(cursor.into_inner()).unwrap());
+
+	let a = parser::infix::formatter::minimal_infix_notation(&tree);
+	println!("{a}");
+
+	let tree = parse("{1 + 2 * 3} / {4 - 5} * {6 + 7} / 2")
+		.unwrap()
+		.0
+		.simplify();
+
+	let mut cursor = std::io::Cursor::<Vec<u8>>::default();
+
+	syntax::dot_writer::write_dot(&mut cursor, &tree).unwrap();
+	println!("{}", String::from_utf8(cursor.into_inner()).unwrap());
+
+	let a = parser::infix::formatter::minimal_infix_notation(&tree);
+	println!("{a}");
 }
