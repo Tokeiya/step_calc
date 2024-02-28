a18e7bca011cc697e76f93cc219e5b7470d686e7
tokeiya <netseed@seedcreate.onmicrosoft.com>
2024-02-28 13:09:35 +0900

    Update html_writer module to support SVG generation and improved HTML writing
    The changes include implementing functions for SVG generation with the help of the 'dot' command and refining HTML writing capabilities. The refactor enhances the html_writer's ability to generate SVG files from formulae and write them into HTML along with improved structural parts such as headers and footers. The corresponding test suite has also been updated to validate the changes made.

--- playground/src/html_writer.rs
+++ playground/src/html_writer.rs
@@ -1,8 +1,9 @@
-use std::io::{Read, Result as IoResult, Write};
+use anyhow::Result as AnyResult;
+use parser::infix::parser::parse;
+use std::io::{Cursor, Read, Result as IoResult, Write};
 use std::ops::Index;
 use std::process::{Command, Stdio};
-
-use anyhow::Result as AnyResult;
+use syntax::dot_writer::{write_dot, WriterError};
 
 pub fn generate_svg(scr: &str) -> IoResult<String> {
 	let mut proc = Command::new("dot")
@@ -40,41 +41,56 @@ pub fn extract_svg_element(scr: &str) -> AnyResult<String> {
 	};
 }
 
-fn write_header(formula: &str, writer: &mut dyn Write) -> IoResult<()> {
-	writer.write(br"<!DOCTYPE html>")?;
-	writer.write(b"\n")?;
+pub fn write_header(formula: &str, writer: &mut dyn Write) -> IoResult<()> {
+	_ = writer.write(br"<!DOCTYPE html>")?;
+	_ = writer.write(b"\n")?;
 
-	writer.write(br#"<html lang="ja">"#)?;
-	writer.write(b"\n")?;
+	_ = writer.write(br#"<html lang="ja">"#)?;
+	_ = writer.write(b"\n")?;
 
-	writer.write(br"<head>")?;
-	writer.write(b"\n")?;
+	_ = writer.write(br"<head>")?;
+	_ = writer.write(b"\n")?;
 
-	writer.write(br#"<meta charset="UTF-8">"#)?;
-	writer.write(b"\n")?;
+	_ = writer.write(br#"<meta charset="UTF-8">"#)?;
+	_ = writer.write(b"\n")?;
 
-	writer.write_fmt(format_args!(r#"<title>{}</title>"#, formula))?;
-	writer.write(b"\n")?;
+	writer.write_fmt(format_args!(r#"<title>{formula}</title>"#))?;
+	_ = writer.write(b"\n")?;
 
-	writer.write(br"</head>")?;
-	writer.write(b"\n")?;
+	_ = writer.write(br"</head>")?;
+	_ = writer.write(b"\n")?;
 
-	writer.write(br"<body>")?;
-	writer.write(b"\n")?;
+	_ = writer.write(br"<body>")?;
+	_ = writer.write(b"\n")?;
 
-	writer.write_fmt(format_args!(r#"<h1>{}</h1>"#, formula))?;
+	writer.write_fmt(format_args!(r#"<h1>{}</h1>"#, formula))
+}
+
+pub fn write_infix_html(formula: &str, writer: &mut dyn Write) -> AnyResult<()> {
+	write_header(formula, writer)?;
+
+	let tree = parse(formula)?.0;
+	let mut cursor = Cursor::<Vec<u8>>::default();
+
+	write_dot(&mut cursor, &tree)?;
+	let dot = String::from_utf8(cursor.into_inner())?;
+
+	let svg = generate_svg(&dot)?;
+
+	let svg = extract_svg_element(&svg)?;
+
+	_ = writer.write(svg.as_bytes())?;
+
+	write_footer(writer)?;
+
 	Ok(())
 }
 
-pub fn write_html(formula: &str, writer: &mut dyn Write) -> IoResult<()> {
-	todo!()
-}
-
 fn write_footer(writer: &mut dyn Write) -> IoResult<()> {
-	writer.write(br"</body>")?;
-	writer.write(b"\n")?;
+	_ = writer.write(br"</body>")?;
+	_ = writer.write(b"\n")?;
 
-	writer.write(br"</html>")?;
+	_ = writer.write(br"</html>")?;
 
 	Ok(())
 }
@@ -89,25 +105,27 @@ pub mod tests {
 	use once_cell::sync::Lazy;
 
 	use super::*;
+	use parser::infix::parser::parse;
+	use syntax::dot_writer::write_dot;
 
-	const EXPECTED_DOT: Lazy<String> = Lazy::new(|| {
-		let mut file = File::open("./test_artifacts/sample.dot").unwrap();
-		let mut str = String::default();
-
-		file.read_to_string(&mut str).unwrap();
-		str
-	});
-
-	const EXPECTED_FULL_SVG: Lazy<String> = Lazy::new(|| {
-		let mut file = File::open("./test_artifacts/sample.svg").unwrap();
-		let mut str = String::default();
-
-		file.read_to_string(&mut str).unwrap();
-		str
-	});
-
-	const EXPECTED_HTML: Lazy<String> = Lazy::new(|| {
+	static EXPECTED_DOT: Lazy<String> = Lazy::new(|| {
 		let mut file = File::open("./test_artifacts/sample.txt").unwrap();
+		let mut str = String::default();
+
+		file.read_to_string(&mut str).unwrap();
+		str
+	});
+
+	static EXPECTED_FULL_SVG: Lazy<String> = Lazy::new(|| {
+		let mut file = File::open("./test_artifacts/sample.txt").unwrap();
+		let mut str = String::default();
+
+		file.read_to_string(&mut str).unwrap();
+		str
+	});
+
+	static EXPECTED_HTML: Lazy<String> = Lazy::new(|| {
+		let mut file = File::open("./test_artifacts/full_html.txt").unwrap();
 		let mut str = String::default();
 
 		file.read_to_string(&mut str).unwrap();
@@ -121,19 +139,44 @@ pub mod tests {
 	}
 
 	#[test]
+	fn generate() {
+		let tree = parse(SAMPLE_FORMULA).unwrap().0;
+
+		let mut cursor = create_cursor();
+		write_dot(&mut cursor, &tree).unwrap();
+
+		let dot = String::from_utf8(cursor.into_inner()).unwrap();
+		let act = generate_svg(&dot).unwrap();
+
+		assert_eq!(&act, EXPECTED_FULL_SVG.as_str())
+	}
+
+	#[test]
+	fn html() {
+		let mut cursor = create_cursor();
+		write_infix_html(SAMPLE_FORMULA, &mut cursor).unwrap();
+
+		let act = String::from_utf8(cursor.into_inner()).unwrap();
+		assert_eq!(&act, EXPECTED_HTML.as_str());
+	}
+
+	#[test]
 	fn extract() {
 		let reg = regex::Regex::new(r"(?s)<svg.*?</svg>").unwrap();
 
-		let binding = EXPECTED_FULL_SVG;
-		let binding = reg.captures(binding.as_str()).unwrap();
+		let binding = EXPECTED_FULL_SVG.as_str();
+		let binding = reg.captures(binding).unwrap();
 		let expected = binding.index(0);
+
+		let act = extract_svg_element(EXPECTED_FULL_SVG.as_str()).unwrap();
+		assert_eq!(&act, expected)
 	}
 
 	#[test]
 	fn header() {
 		let reg = regex::Regex::new(r"(?s)^<!DOCTYPE.*?</h1>").unwrap();
-		let binding = EXPECTED_HTML;
-		let cap = reg.captures(binding.as_str());
+		let binding = EXPECTED_HTML.as_str();
+		let cap = reg.captures(binding);
 		let expected = cap.unwrap().index(0).to_string();
 
 		let mut cursor = create_cursor();
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -13,38 +13,48 @@ use syntax::dot_writer::write_dot;
 
 mod html_writer;
 
+use crate::html_writer::{extract_svg_element, generate_svg, write_header};
+use html_writer::write_infix_html;
+
 fn main() {
-	let tree = parse("{1+2*3}/{{4-5}*{{6+7}/2}}").unwrap().0.simplify();
-	let tree = tree.simplify();
+	const formula: &str = "{1+2*3}/{{4-5}*{{6+7}/2}}";
 
-	let mut cursor = Cursor::<Vec<u8>>::default();
+	let mut writer = Cursor::<Vec<u8>>::default();
+	//write_html(&formula, &mut cursor).unwrap();
 
-	_ = write_dot(&mut cursor, &tree).unwrap();
+	write_header(formula, &mut writer).unwrap();
 
-	let str = String::from_utf8(cursor.into_inner()).unwrap();
-	println!("{}", &str)
+	let svg = generate_svg(formula).unwrap();
+
+	println!("{}", &svg);
+
+	//let svg = extract_svg_element(&svg).unwrap();
+
+	//_ = writer.write(svg.as_bytes()).unwrap();
+
+	//crate::html_writer::write_footer(writer)?;
 }
 
-fn write_samples() {
-	let current_dir = env::current_dir().unwrap();
-	println!("The current directory is {}", current_dir.display());
-	let tree = parse("{1+2*3}/{{4-5}*{{6+7}/2}}").unwrap().0.simplify();
-	{
-		let mut file = File::create("./playground/test_artifacts/sample.dot").unwrap();
-		write_dot(&mut file, &tree).unwrap();
-	}
-
-	{
-		let mut proc = Command::new("dot")
-			.args(&[
-				r"-Tsvg",
-				r".\playground\test_artifacts\sample.dot",
-				r"-o",
-				r".\playground\test_artifacts\sample.svg",
-			])
-			.output()
-			.unwrap();
-
-		println!("{:?}", proc)
-	}
-}
+// fn write_samples() {
+// 	let current_dir = env::current_dir().unwrap();
+// 	println!("The current directory is {}", current_dir.display());
+// 	let tree = parse("{1+2*3}/{{4-5}*{{6+7}/2}}").unwrap().0.simplify();
+// 	{
+// 		let mut file = File::create("./playground/test_artifacts/sample.dot").unwrap();
+// 		write_dot(&mut file, &tree).unwrap();
+// 	}
+//
+// 	{
+// 		let mut proc = Command::new("dot")
+// 			.args(&[
+// 				r"-Tsvg",
+// 				r".\playground\test_artifacts\sample.dot",
+// 				r"-o",
+// 				r".\playground\test_artifacts\sample.svg",
+// 			])
+// 			.output()
+// 			.unwrap();
+//
+// 		println!("{:?}", proc)
+// 	}
+// }
