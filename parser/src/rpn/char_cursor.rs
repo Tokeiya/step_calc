f111b2525fa4600e5783fdd36f40d4c2691149c8
tokeiya <netseed@seedcreate.onmicrosoft.com>
2024-02-29 18:16:43 +0900

    Refactor CharCursor and improve test cases
    The CharCursor struct was refactored to eliminate the separate iterator structure, and instead embed iterator capabilities within CharCursor. New methods were added, including a conversion from strings, a method to get remaining characters, and one to obtain consumed characters as strings. Test cases were significantly expanded to capture and verify this new functionality.

--- parser/src/rpn/char_cursor.rs
+++ parser/src/rpn/char_cursor.rs
@@ -3,36 +3,7 @@ pub struct CharCursor {
 	pos: usize,
 }
 
-pub struct CursorIterator<'a> {
-	slice: &'a [char],
-	idx: usize,
-}
-
-impl<'a> CursorIterator<'a> {
-	fn new(scr: &'a [char]) -> Self {
-		CursorIterator { slice: scr, idx: 0 }
-	}
-}
-
-impl<'a> Iterator for CursorIterator<'a> {
-	type Item = char;
-
-	fn next(&mut self) -> Option<Self::Item> {
-		if (self.idx >= self.slice.len()) {
-			None
-		} else {
-			let c = self.slice[self.idx];
-			self.idx += 1;
-			Some(c)
-		}
-	}
-}
-
 impl CharCursor {
-	pub fn iter(&self) -> CursorIterator<'_> {
-		CursorIterator::new(&self.vec[self.pos..])
-	}
-
 	pub fn next(&mut self) -> Option<char> {
 		if (self.vec.len() <= self.pos) {
 			None
@@ -44,20 +15,139 @@ impl CharCursor {
 	}
 
 	pub fn previous(&mut self) -> Option<char> {
+		if self.pos == 0 {
+			None
+		} else {
+			self.pos -= 1;
+			Some(self.vec[self.pos])
+		}
+	}
+
+	pub fn remainder_to_string(&self) -> String {
+		todo!()
+	}
+
+	pub fn consumed_to_string(&self) -> String {
 		todo!()
 	}
 }
 
 impl From<&str> for CharCursor {
 	fn from(value: &str) -> Self {
-		todo!()
+		CharCursor {
+			vec: value.chars().collect(),
+			pos: 0,
+		}
 	}
 }
 
 #[cfg(test)]
 mod tests {
+	use super::*;
+
+	const SAMPLE: &str = "abcdefghijklmnopqrstuvwxyz";
+
 	#[test]
-	fn cursor_init() {
-		todo!()
+	fn cursor_init_test() {
+		let fixture = CharCursor::from(SAMPLE);
+		assert_eq!(fixture.pos, 0);
+
+		let expected: Vec<_> = SAMPLE.chars().collect();
+
+		assert_eq!(fixture.vec.len(), expected.len());
+
+		for (idx, (act, exp)) in fixture.vec.iter().zip(expected.iter()).enumerate() {
+			assert_eq!(act, exp, "{idx} mismatch")
+		}
+	}
+
+	#[test]
+	fn next_test() {
+		let mut fixture = CharCursor::from(SAMPLE);
+		let expected: Vec<_> = SAMPLE.chars().collect();
+
+		for e in expected.iter() {
+			let a = fixture.next().unwrap();
+			assert_eq!(&a, e);
+		}
+
+		for _ in 0..10 {
+			assert_eq!(fixture.pos, expected.len());
+			assert!(fixture.next().is_none());
+		}
+	}
+
+	#[test]
+	fn previous_test() {
+		let mut fixture = CharCursor::from(SAMPLE);
+		let expected: Vec<_> = SAMPLE.chars().rev().collect();
+
+		while fixture.next().is_some() {}
+
+		for e in expected.iter() {
+			let a = fixture.previous().unwrap();
+			assert_eq!(&a, e)
+		}
+
+		for _ in 0..10 {
+			assert_eq!(fixture.pos, 0);
+			assert!(fixture.previous().is_none());
+		}
+	}
+
+	#[test]
+	fn remainder_to_string_test() {
+		let mut fixture = CharCursor::from(SAMPLE);
+
+		for i in 0..SAMPLE.len() {
+			let actual = fixture.remainder_to_string();
+			assert_eq!(&actual, &SAMPLE[i..]);
+
+			let mut consumed = fixture.consumed_to_string();
+			consumed.push_str(&actual);
+			assert_eq!(&consumed, SAMPLE)
+		}
+
+		for _ in 0..10 {
+			let actual = fixture.remainder_to_string();
+			assert_eq!(&actual, "");
+		}
+
+		for i in (0..SAMPLE.len()).rev() {
+			let actual = fixture.remainder_to_string();
+			assert_eq!(&actual, &SAMPLE[i..]);
+
+			let mut consumed = fixture.consumed_to_string();
+			consumed.push_str(&actual);
+			assert_eq!(&consumed, SAMPLE);
+		}
+	}
+
+	#[test]
+	fn consumed_to_string_test() {
+		let mut fixture = CharCursor::from(SAMPLE);
+
+		for i in 0..SAMPLE.len() {
+			let exp = &SAMPLE[..i];
+			let mut consumed = fixture.consumed_to_string();
+			assert_eq!(&consumed, exp);
+
+			consumed.push_str(&fixture.remainder_to_string());
+			assert_eq!(&consumed, SAMPLE)
+		}
+
+		for _ in 0..10 {
+			let actual = fixture.consumed_to_string();
+			assert_eq!(&actual, SAMPLE)
+		}
+
+		for i in (0..SAMPLE.len()).rev() {
+			let exp = &SAMPLE[..i];
+			let mut consumed = fixture.consumed_to_string();
+			assert_eq!(&consumed, exp);
+
+			consumed.push_str(&fixture.remainder_to_string());
+			assert_eq!(&consumed, SAMPLE)
+		}
 	}
 }
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -5,16 +5,9 @@ use std::io::Cursor;
 mod html_writer;
 
 fn main() {
-	const INPUT: &str = r"10 20    30 40  + -        *";
+	let str = "0123456789";
+	let slice = &str[..0];
+	println!("{slice}");
 
-	let mut cursor = Cursor::<Vec<char>>::new(INPUT.chars().collect());
-
-	let reg = Regex::new(r"\s+").unwrap();
-	let array: Vec<_> = reg.split(INPUT).collect();
-
-	println!("{}", array.len());
-
-	for elem in array.iter() {
-		println!("{}", elem)
-	}
+	for i in (0..10).rev() {}
 }
