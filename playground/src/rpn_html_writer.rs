8a6b691d21cb4bac657967bea8ec6db67168b0e2
tokeiya3@notebook <netseed@seedcreate.onmicrosoft.com>
2024-03-03 13:34:51 +0900

    Lunch sync.

--- playground/src/rpn_html_writer.rs
+++ playground/src/rpn_html_writer.rs
@@ -9,7 +9,7 @@ use syntax::number_value::NumberValue;
 
 fn token_to_string(scr: &VecDeque<Token>) -> String {
 	let mut buff = String::default();
-	
+
 	for elem in scr.iter().rev() {
 		match elem {
 			Token::Number(num) => match num {
@@ -22,14 +22,14 @@ fn token_to_string(scr: &VecDeque<Token>) -> String {
 				Operation::Div => buff.push('/'),
 			},
 		}
-		
+
 		buff.push(' ');
 	}
 	buff.remove(buff.len() - 1);
 	buff
 }
 
-fn write_header(input: &dyn Iterator<Item=Token>, writer: &mut dyn Write) -> AnyResult<()> {
+fn write_header(input: &VecDeque<Token>, writer: &mut dyn Write) -> AnyResult<()> {
 	todo!()
 }
 
@@ -41,23 +41,60 @@ fn write_footer(writer: &mut dyn Write) -> AnyResult<()> {
 	todo!()
 }
 
+pub fn write_html<T: Write>(input: &str, mut writer: T) -> AnyResult<()> {
+	todo!()
+}
+
 #[cfg(test)]
 mod tests {
 	use parser::rpn as Rpn;
-	
+	use std::io::Cursor;
+
 	use super::*;
-	
+
+	fn create_cursor() -> Cursor<Vec<u8>> {
+		Cursor::<Vec<u8>>::default()
+	}
 	fn gen_token_stream() -> VecDeque<Token> {
 		let (ret, rem) = Rpn::parser::tokenize("4 2 3 4 5 / + * -");
 		assert!(rem.is_empty());
 		ret
 	}
-	
+
 	#[test]
 	fn token_to_string_test() {
 		let tokens = gen_token_stream();
 		let act = token_to_string(&tokens);
-		
+
 		assert_eq!(&act, "4 2 3 4 5 / + * -");
+	}
+
+	#[test]
+	fn write_header_test() {
+		const EXPECTED: &str = r#"<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
+    <title>4 2 3 4 5 / + * -</title>
+</head>
+<body>
+<table>
+"#;
+		let mut cursor = create_cursor();
+		write_header(&gen_token_stream(), &mut cursor).unwrap();
+
+		todo!()
 	}
 }
