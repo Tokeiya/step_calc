fcfb3cfb12c9a729bf4def26f3fdebbf88f7b1d6
net_s <netseed@seedcreate.onmicrosoft.com>
2024-03-03 23:57:19 +0900

    Implement option parser and improve HTML writer
    The playground now parses command line arguments and generates proper commit messages. Additionally, HTML writing was improved to present calculations for arithmetic expressions. Test attributes were configured and unnecessary code execution was prevented, culminating in overall better performance and readability of code.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,32 +1,49 @@
+use std::env;
 use std::fs::File;
-use parser::rpn::parser::Token;
-use syntax::binary_operation::Operation;
-use rpn_html_writer::write_html;
+
+use anyhow::{Error as AnyError, Result as AnyResult};
+
+use option_parser::*;
 
 #[allow(dead_code)]
 mod infix_html_writer;
 mod rpn_html_writer;
 mod test_helper;
 
+mod option_parser;
 #[cfg(test)]
 mod test_writer;
 
-#[allow(dead_code)]
-fn print(token: &Token) {
-	match token {
-		Token::Number(num) => println!("{:?}", num),
-		Token::Operator(op) => match op {
-			Operation::Add => println!("+"),
-			Operation::Sub => println!("-"),
-			Operation::Mul => println!("*"),
-			Operation::Div => println!("/"),
-		},
-	}
-}
+fn main() -> AnyResult<()> {
+    // let file=File::create("rpn_first_light.html").unwrap();
+    //
+    // write_html("16 8 4 2 - * +",&file).unwrap()
 
-fn main() {
-	let file=File::create("rpn_first_light.html").unwrap();
-	
-	write_html("16 8 4 2 - * +",&file).unwrap()
-	
+    let args: Vec<String> = env::args().collect();
+    // let mut args = Vec::<String>::default();
+    //
+    // args.push("F:\\step_calc\\target\\debug\\playground.exe".to_string());
+    // args.push("-r".to_string());
+    // args.push("10 20 +".to_string());
+    // args.push("-o".to_string());
+    // args.push("output.html".to_string());
+
+    println!("{:?}", &args);
+
+    let opt = parse_command_options(args)?;
+
+    println!("{:?}", &opt);
+
+    if opt.output_path().is_none() {
+        println!("Output path is not specified.");
+        return Err(AnyError::msg("Output path is not specified."));
+    }
+
+    let file = File::create(opt.output_path().unwrap())?;
+
+    let a = &opt.rpn_expression().unwrap().replace('"', "");
+    //println!("input:{a}");
+    rpn_html_writer::write_html(a, file)?;
+
+    Ok(())
 }
