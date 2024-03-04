aac48cbc7f798f46b33769dfb3cfc321e7a21397
tokeiya3@notebook <netseed@seedcreate.onmicrosoft.com>
2024-03-04 18:43:29 +0900

    sync

--- list_dumper/src/type_descriptor.rs
+++ list_dumper/src/type_descriptor.rs
@@ -1,9 +1,12 @@
 pub use super::column_info::{Align, ColumnInfo};
 
-pub trait TypeDescriptor {
-	fn columns() -> Vec<ColumnInfo> {
+pub trait TypeDescriptor<const N:usize> {
+	fn columns() -> [ColumnInfo;N] {
 		todo!()
 	}
-
+	
+	fn fill(&self,buffer:&mut [String;N])->bool{
+		todo!()
+	}
 
 }
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -14,6 +14,30 @@ mod option_parser;
 #[cfg(test)]
 mod test_writer;
 
-fn main() -> AnyResult<()> {
-    rpn_html_writer::procedure()
+fn bar<const N:usize>()->[i32;N]{
+    let mut a=[0;N];
+    
+    for i in 0..N {
+        a[i]=(i+10) as i32;
+    }
+    
+    for elem in a.iter_mut() {
+        *elem+=100;
+    }
+    
+    a
+
+}
+
+fn main() {
+    let mut v=Vec::<String>::new();
+    v.push("hello".to_string());
+    v.push("world".to_string());
+    foo(v.iter().map(|x|x.as_str()));
+}
+
+fn foo<'a>(iter:impl Iterator<Item=&'a str>){
+    for elem in iter {
+        println!("{elem}")
+    }
 }
