097a846435bc093ecc0d1cb9a99113d40c893527
Tokeiya <netseed@seedcreate.onmicrosoft.com>
2024-03-06 08:23:46 +0900

    sync

--- list_dumper/src/type_descriptor.rs
+++ list_dumper/src/type_descriptor.rs
@@ -1,12 +1,37 @@
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
 pub use super::column_info::{Align, ColumnInfo};
 
-pub trait TypeDescriptor<const N:usize> {
-	fn columns() -> [ColumnInfo;N] {
-		todo!()
-	}
-	
-	fn fill(&self,buffer:&mut [String;N])->bool{
-		todo!()
-	}
+pub enum ExprssionError{};
 
+impl Debug for ExprssionError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl Display for ExprssionError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl Error for ExprssionError{
+    
+}
+
+pub trait TypeDescriptor<const N: usize> {
+    fn columns() -> &'static [ColumnInfo; N] {
+        todo!()
+    }
+
+    fn fill(&self, buffer: &mut [String; N]) -> Result<(),ExprssionError> {
+        todo!()
+    }
+    
+    fn record(&self)->Result<Vec<String>,ExprssionError>{
+        todo!()
+    }
+    
+    
 }
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,43 +1,19 @@
-use std::env;
-use std::fs::File;
-
-use anyhow::{Error as AnyError, Result as AnyResult};
-
-use option_parser::*;
+use std::fmt::{Arguments, Debug, Display};
 
 #[allow(dead_code)]
 mod infix_html_writer;
 mod rpn_html_writer;
 mod test_helper;
 
+mod console_color;
 mod option_parser;
 #[cfg(test)]
 mod test_writer;
 
-fn bar<const N:usize>()->[i32;N]{
-    let mut a=[0;N];
-    
-    for i in 0..N {
-        a[i]=(i+10) as i32;
-    }
-    
-    for elem in a.iter_mut() {
-        *elem+=100;
-    }
-    
-    a
-
-}
-
 fn main() {
-    let mut v=Vec::<String>::new();
-    v.push("hello".to_string());
-    v.push("world".to_string());
-    foo(v.iter().map(|x|x.as_str()));
+	foo(format_args!("{}", "hoge"));
 }
 
-fn foo<'a>(iter:impl Iterator<Item=&'a str>){
-    for elem in iter {
-        println!("{elem}")
-    }
+fn foo(arg: Arguments<'_>) {
+	println!("{}:{}", "env", arg)
 }
