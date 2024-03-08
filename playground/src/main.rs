6100ae0258b48506e8e4ad804a5b85ec3f7675c5
tokeiya3@notebook <netseed@seedcreate.onmicrosoft.com>
2024-03-08 12:17:11 +0900

    resolve

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,17 +1,34 @@
-use std::fmt::{Arguments, Debug, Display};
-
-#[allow(dead_code)]
-mod infix_html_writer;
-mod rpn_html_writer;
-mod test_helper;
+#[cfg(test)]
+use dashmap::DashMap;
+#[cfg(test)]
+use once_cell::sync::Lazy;
 
 #[cfg(test)]
-mod test_writer;
+static ACTUAL: Lazy<DashMap<usize, String>> = Lazy::new(|| DashMap::default());
 
-fn main() {
-	let mut str = String::default();
+macro_rules! println {
+    ($($arg:tt)*) => {
+        #[cfg(not(test))]
+        std::println!($($arg)*)
+
+        // 通常時はprintln!をそのまま呼び出す
+        #[cfg(test)]
+        {
+            let formatted_message = format!($($arg)*); // テスト時にはメッセージをフォーマット
+            // フォーマットされたメッセージを利用する。例えば、標準出力に出力する、ファイルに記録する、アサーションで検証するなど。
+            std::println!("TestOutput: {}", formatted_message) // 例: テスト出力用に加工したメッセージを出力
+        }
+    };
 }
 
-fn foo(arg: Arguments<'_>) {
-	println!("{}:{}", "env", arg)
+// マクロの使用例
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn test_conditional_return() {
+		let mut a = once_cell::sync::OnceCell::<i32>::new();
+		let b = a.set(20);
+	}
 }
