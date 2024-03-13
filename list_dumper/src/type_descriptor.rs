3dcb95ba331a323d9454f826b86ef4dbd663ec67
net_s <netseed@seedcreate.onmicrosoft.com>
2024-03-13 20:33:03 +0900

    Refactor type descriptors and remove unused trait
    Type descriptors are refactored to use standard library's Error trait and provide more structured return types. Also, unused trait 'Hoge' has been removed. The 'describe' method of both InnerDescriptor and OuterDescriptor traits were changed to return a DescribeResult instead of a vector of tuples. This results in a more precise type specification.

--- list_dumper/src/type_descriptor.rs
+++ list_dumper/src/type_descriptor.rs
@@ -1,16 +1,19 @@
-use super::alignment::Alignment;
+use std::error::Error as StdError;
+use std::fmt::{Debug, Display};
+
+pub type DescribeElement<T> = Result<Option<String>, T>;
+pub type DescribeResult<T, U, const N: usize> = Result<Box<[DescribeElement<U>; N]>, T>;
 
 pub trait InnerDescriptor {
 	const SIZE: usize;
-	fn describe(&self) -> Vec<(Option<String>, Alignment)>;
+
+	fn header() -> Box<[&'static str; Self::SIZE]>;
+	fn describe<T: StdError, U: StdError>(&self) -> DescribeResult<T, U, { Self::SIZE }>;
 }
 
 pub trait OuterDescriptor {
 	const SIZE: usize;
-	fn describe(value: &Self) -> Vec<(Option<String>, Alignment)>;
-}
 
-pub trait Hoge {
-	const S: usize;
-	fn foo() -> [u8; Self::S];
+	fn header() -> Box<[&'static str; Self::SIZE]>;
+	fn describe<T: StdError, U: StdError>(value: &Self) -> DescribeResult<T, U, { Self::SIZE }>;
 }
