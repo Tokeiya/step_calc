a3d93b8fa94c94d9a63f21b42d3cb16428aa3156
tokeiya <netseed@seedcreate.onmicrosoft.com>
2024-03-18 12:24:49 +0900

    The provided commit message doesn't match the given diff. Here's the correct commit message:
    Commit message:
    Rename 'list_dumper' module to 'type_description'
    
    The 'list_dumper' module has been renamed to 'type_description' to better reflect its purpose. All dependencies and references to the module in the project's Cargo.toml, Cargo.lock, and other related files have also been updated accordingly.

--- list_dumper/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![feature(generic_const_exprs)]
-#![feature(associated_const_equality)]
-
-extern crate core;
-
-mod alignment;
-mod type_descriptor;
---
+++ type_description/src/lib.rs
@@ -0,0 +1,7 @@
+#![feature(generic_const_exprs)]
+#![feature(associated_const_equality)]
+
+extern crate core;
+
+mod alignment;
+mod type_descriptor;
