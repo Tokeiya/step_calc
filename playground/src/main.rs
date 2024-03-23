a932b8c20cf39a5c91cd02c7c1ea7f68de563a1f
net_s <netseed@seedcreate.onmicrosoft.com>
2024-03-23 23:35:00 +0900

    Add `anyhow` and `combine` dependencies, remove unused traits and structs
    This update adds `anyhow` and `combine` dependencies to the Cargo.toml file. Unnecessary trait and struct definitions (`Foo`, `Bar`, `Hoge`) have been removed from the main.rs file. A new feature flag for handling incomplete features has also been introduced.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,21 +1,6 @@
+#![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 #![feature(associated_const_equality)]
 
-use std::alloc::{alloc, Layout};
-use std::marker::PhantomData;
-use std::ptr;
 
-trait Foo{
-	const N:usize;
-	type Item;
-}
-
-trait Bar{
-	const N:usize;
-	type Item;
-}
-
-pub struct Hoge<const N:usize,T,U:Foo<N={N},Item=T>,V:Bar<N={N},Item=T>>(PhantomData<T>,PhantomData<U>,PhantomData<V>);
-
-fn main() {
-}
+fn main() {}
